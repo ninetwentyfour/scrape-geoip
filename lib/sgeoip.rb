e33c5141117b8d03074f468660c61f893f9204b9
Travis Berry <contact@travisberry.com>
2012-09-26 17:22:28 -0600

    added some error handling

--- lib/sgeoip.rb
+++ lib/sgeoip.rb
@@ -5,14 +5,18 @@ require 'open-uri'
 module Sgeoip
   def self.scrape(ip)
     url = "http://www.geoiptool.com/en/?IP=#{ip}"
-    doc = Nokogiri::HTML(open(url))
-    geoip = {  "host"         => doc.at_css(".tbl_style tr:nth-child(1) td.arial_bold").text,
-               "country"      => doc.at_css(".tbl_style tr:nth-child(3) td.arial_bold").text,
-               "country_code" => doc.at_css(".tbl_style tr:nth-child(4) td.arial_bold").text,
-               "region"       => doc.at_css(".tbl_style tr:nth-child(5) td.arial_bold").text,
-               "city"         => doc.at_css(".tbl_style tr:nth-child(6) td.arial_bold").text,
-               "zip"          => doc.at_css(".tbl_style tr:nth-child(7) td.arial_bold").text
-             }
+    begin
+      doc = Nokogiri::HTML(open(url))
+      geoip = {  "host"         => doc.at_css(".tbl_style tr:nth-child(1) td.arial_bold").text,
+                 "country"      => doc.at_css(".tbl_style tr:nth-child(3) td.arial_bold").text,
+                 "country_code" => doc.at_css(".tbl_style tr:nth-child(4) td.arial_bold").text,
+                 "region"       => doc.at_css(".tbl_style tr:nth-child(5) td.arial_bold").text,
+                 "city"         => doc.at_css(".tbl_style tr:nth-child(6) td.arial_bold").text,
+                 "zip"          => doc.at_css(".tbl_style tr:nth-child(7) td.arial_bold").text
+               }
+    rescue Exception => e
+      geoip = { "host" => ""}
+    end
     if geoip["host"].empty?
       geoip["error"] = "Results Not Found"
     end
